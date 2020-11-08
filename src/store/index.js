87fe568e9246cbff6603c14c878b57069375b617
Tomáš Reinert <reinerttomas@gmail.com>
2020-11-08 16:52:19 +0100

    add vuexfire

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import firebaseMutations from "vuexfire";
 import auth from "./modules/auth";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  mutations: firebaseMutations,
   modules: {
-    auth
-  }
+    auth,
+  },
 });
